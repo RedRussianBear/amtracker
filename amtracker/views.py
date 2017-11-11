674a2953b5c12844bce4c1d8fbdb8e4561a65afa
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-10 19:06:22 -0500

    Removed body

--- amtracker/views.py
+++ amtracker/views.py
@@ -5,6 +5,4 @@ from amtracker import amtracker
 @amtracker.route('/')
 @amtracker.route('/index')
 def index():
-    alumni = [{'name': 'Kalin', 'city': 'College Park', 'class': 2016},
-              {'name': 'Ethan', 'city': 'New York', 'class': 2017}]
-    return render_template('index.html', alumni=alumni)
+    return render_template('index.html')
