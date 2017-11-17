76a76707c48dec0ac0c7ae645fbe32de7620446b
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:02:59 -0500

    Fixed raspi script

--- raspi.py
+++ raspi.py
@@ -1,13 +1,13 @@
 import serial
 import json
-import urllib3
+from urllib.request import urlopen
 from time import sleep
 
-data = json.load(urllib3.urlopen('http://someurl/path/to/json'))
+data = json.load(urlopen('http://someurl/path/to/json'))
 ser = serial.Serial('/dev/tty.usbserial', 9600)
 
 while True:
-    data = json.load(urllib3.urlopen('http://someurl/path/to/json'))
+    data = json.load(urlopen('http://someurl/path/to/json'))
     ser.write(b'1:%d' % int(data['motor0']))
     sleep(.01)
     ser.write(b'2:%d' % int(data['motor1']))
