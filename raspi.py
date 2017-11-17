660501761955c7be352ce7d0dcec3dfe93ef0a91
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:11:21 -0500

    Added cast

--- raspi.py
+++ raspi.py
@@ -6,7 +6,7 @@ from time import sleep
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-    data = json.load(urlopen('http://demo.mkhrenov.com/state'))
+    data = json.load(str(urlopen('http://demo.mkhrenov.com/state')))
     ser.write(b'1:%d' % int(data['motor0']))
     sleep(.01)
     ser.write(b'2:%d' % int(data['motor1']))
