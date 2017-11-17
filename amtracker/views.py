9620661497142bf26addf2adaee6d86ea1dd49b5
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 08:56:53 -0500

    Repurposed for IOT demo

--- amtracker/views.py
+++ amtracker/views.py
@@ -1,8 +1,24 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from amtracker import amtracker
 
+motor_states = {'motor1': 0, 'motor2': 0}
 
-@amtracker.route('/')
+
+@amtracker.route('/', methods=['POST', 'GET'])
 @amtracker.route('/index')
 def index():
     return render_template('index.html')
+
+
+@amtracker.route('/update', methods=['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        motor_states['motor1'] = int(request.form['motor1'])
+        motor_states['motor2'] = int(request.form['motor2'])
+        print(request.form)
+    return 'Good!'
+
+
+@amtracker.route('/state')
+def state():
+    return jsonify(motor_states)
