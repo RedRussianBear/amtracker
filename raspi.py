bcf6cc2653a2efda58a1bb8dc3ed92a71c919790
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:48:59 -0500

    Debugging

--- raspi.py
+++ raspi.py
@@ -8,9 +8,11 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     data = json.load(urlopen('http://demo.mkhrenov.com/state'))
     ser.write('1:%d' % int(data['motor1']))
+    print('1:%d' % int(data['motor1']))
     print(ser.readline())
     sleep(3)
     ser.write('2:%d' % int(data['motor2']))
+    print('2:%d' % int(data['motor2']))
     print(ser.readline())
     sleep(3)
 
