13af45fd9cd3e93627edaad5d40b9b3b558d53de
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:23:35 -0500

    Added line endings

--- raspi.py
+++ raspi.py
@@ -8,10 +8,10 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     r = urlopen('http://demo.mkhrenov.com/state')
     data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
-    ser.write(bytes('1:%d' % int(data['motor1']), 'ascii'))
+    ser.write(bytes('1:%d\n' % int(data['motor1']), 'ascii'))
     print(bytes('1:%d' % int(data['motor1']), 'ascii'))
     sleep(.01)
-    ser.write(bytes('2:%d' % int(data['motor2']), 'ascii'))
+    ser.write(bytes('2:%d\n' % int(data['motor2']), 'ascii'))
     print(bytes('2:%d' % int(data['motor2']), 'ascii'))
     sleep(1)
 
