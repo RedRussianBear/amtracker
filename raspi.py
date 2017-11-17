8fefd043584b2702c635d2823d050d02dc2e5f85
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:26:36 -0500

    Moved to python2

--- raspi.py
+++ raspi.py
@@ -1,6 +1,6 @@
 import serial
 import json
-from urllib.request import urlopen
+from urllib2 import urlopen
 from time import sleep
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
@@ -8,11 +8,11 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     r = urlopen('http://demo.mkhrenov.com/state')
     data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
-    ser.write(bytes('1:%d\n' % int(data['motor1']), 'ascii'))
-    print(bytes('1:%d' % int(data['motor1']), 'ascii'))
+    ser.write('1:%d' % int(data['motor1']))
+    print('1:%d' % int(data['motor1']))
     sleep(.01)
-    ser.write(bytes('2:%d\n' % int(data['motor2']), 'ascii'))
-    print(bytes('2:%d' % int(data['motor2']), 'ascii'))
+    ser.write('2:%d' % int(data['motor2']))
+    print('1:%d' % int(data['motor1']))
     sleep(1)
 
 
