b620c4d4c0d4e5e35feebb0e3d6309cab9ee1d6b
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:08:24 -0500

    Fixed URL

--- raspi.py
+++ raspi.py
@@ -3,11 +3,10 @@ import json
 from urllib.request import urlopen
 from time import sleep
 
-data = json.load(urlopen('http://someurl/path/to/json'))
 ser = serial.Serial('/dev/tty.usbserial', 9600)
 
 while True:
-    data = json.load(urlopen('http://someurl/path/to/json'))
+    data = json.load(urlopen('http://demo.mkhrenov.com/state'))
     ser.write(b'1:%d' % int(data['motor0']))
     sleep(.01)
     ser.write(b'2:%d' % int(data['motor1']))
