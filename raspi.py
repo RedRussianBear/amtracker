c7622b56a9ed32d7910f4bc11c9ac4dc531dc848
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-17 10:15:16 -0500

    Fixed keys

--- raspi.py
+++ raspi.py
@@ -8,9 +8,9 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     r = urlopen('http://demo.mkhrenov.com/state')
     data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
-    ser.write(b'1:%d' % int(data['motor0']))
+    ser.write(b'1:%d' % int(data['motor1']))
     sleep(.01)
-    ser.write(b'2:%d' % int(data['motor1']))
+    ser.write(b'2:%d' % int(data['motor2']))
     sleep(.25)
 
 
